0c1b6a67ca1533eeb85423b741f27457d91a76ec
Peter Lacko <peter.lacko@gooddata.com>
2018-10-03 14:49:03 +0200

    RELATED: ONE-3353 Support color palette in SDK visualizations

--- src/components/visualizations/utils/color.ts
+++ src/components/visualizations/utils/color.ts
@@ -1,10 +1,12 @@
 // (C) 2007-2018 GoodData Corporation
+import { IColorPalette, IColorPaletteItem } from '../chart/Chart';
+
 export const WHITE = 'rgb(255, 255, 255)';
 export const BLACK = 'rgb(0, 0, 0)';
 export const GRAY = 'rgb(201, 213, 223)';
 export const AXIS_LINE_COLOR = '#d5d5d5';
 
-export const DEFAULT_COLOR_PALETTE = [
+export const DEFAULT_COLORS = [
     'rgb(20,178,226)',
     'rgb(0,193,141)',
     'rgb(229,77,66)',
@@ -28,6 +30,109 @@ export const DEFAULT_COLOR_PALETTE = [
     'rgb(136,219,244)',
     'rgb(189,234,222)',
     'rgb(239,197,194)'
+];
+
+export const DEFAULT_COLOR_PALETTE = [
+    {
+        guid: 'blue',
+        fill: { r: 20, g: 178, b: 226 }
+    // 'rgb(20,178,226)',
+    },
+    {
+        guid: 'green',
+        fill: { r: 0, g: 193, b: 141 }
+    // 'rgb(0,193,141)',
+    },
+    {
+        guid: 'red',
+        fill: { r: 229, g: 77, b: 66 }
+    // 'rgb(229,77,66)',
+    },
+    {
+        guid: 'orange',
+        fill: { r: 241, g: 134, b: 0 }
+    // 'rgb(241,134,0)',
+    },
+    {
+        guid: 'purple',
+        fill: { r: 171, g: 85, b: 163 }
+    // 'rgb(171,85,163)',
+    },
+    {
+        guid: 'yellow',
+        fill: { r: 244, g: 213, b: 33 }
+    // 'rgb(244,213,33)',
+    },
+    {
+        guid: 'grey',
+        fill: { r: 148, g: 161, b: 174 }
+    // 'rgb(148,161,174)',
+    },
+    {
+        guid: 'blue-light',
+        fill: { r: 107, g: 191, b: 216 }
+    // 'rgb(107,191,216)',
+    },
+    {
+        guid: 'violet-light',
+        fill: { r: 181, g: 136, b: 177 }
+    // 'rgb(181,136,177)',
+    },
+    {
+        guid: 'red-light',
+        fill: { r: 238, g: 135, b: 128 }
+    // 'rgb(238,135,128)',
+    },
+    {
+        guid: 'orange-light',
+        fill: { r: 241, g: 171, b: 84 }
+    // 'rgb(241,171,84)',
+    },
+    {
+        guid: 'green-light',
+        fill: { r: 133, g: 209, b: 188 }
+    // 'rgb(133,209,188)',
+    },
+    {
+        guid: 'blue-dark',
+        fill: { r: 41, g: 117, b: 170 }
+    // 'rgb(41,117,170)',
+    },
+    {
+        guid: 'green-dark',
+        fill: { r: 4, g: 140, b: 103 }
+    // 'rgb(4,140,103)',
+    },
+    {
+        guid: 'red-dark',
+        fill: { r: 181, g: 60, b: 51 }
+    // 'rgb(181,60,51)',
+    },
+    {
+        guid: 'orange-dark',
+        fill: { r: 163, g: 101, b: 46 }
+    // 'rgb(163,101,46)',
+    },
+    {
+        guid: 'purple-dark',
+        fill: { r: 140, g: 57, b: 132 }
+    // 'rgb(140,57,132)',
+    },
+    {
+        guid: 'azure',
+        fill: { r: 136, g: 219, b: 244 }
+    // 'rgb(136,219,244)',
+    },
+    {
+        guid: 'green-celadon',
+        fill: { r: 189, g: 234, b: 222 }
+    // 'rgb(189,234,222)',
+    },
+    {
+        guid: 'pink-pale',
+        fill: { r: 239, g: 197, b: 194 }
+    // 'rgb(239,197,194)'
+    }
 ];
 
 export const HEATMAP_BLUE_COLOR_PALETTE = [
@@ -78,4 +183,28 @@ export function normalizeColorToRGB(color: string) {
     return color.replace(hexPattern, (_prefix: string, r: string, g: string, b: string) => {
         return `rgb(${[r, g, b].map(value => (parseInt(value, 16).toString(10))).join(', ')})`;
     });
+}
+
+export function getColorPaletteFromColors(colors: string[]): IColorPalette {
+    try {
+        return colors.map((color: string, index: number) => {
+            const match = color.match(/^rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i);
+            if (match) {
+                return {
+                    guid: String(index),
+                    fill: {
+                        r: Number(match[1]),
+                        g: Number(match[2]),
+                        b: Number(match[3])
+                    }
+                };
+            }
+        });
+    } catch (_ignored) {
+        return DEFAULT_COLOR_PALETTE;
+    }
+}
+
+export function getRgbString(color: IColorPaletteItem): string {
+    return `rgb(${color.fill.r},${color.fill.g},${color.fill.b})`;
 }
